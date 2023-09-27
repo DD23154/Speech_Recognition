d158aeeb26d046a1d3f22d3d8b06478d1f723cfa
DD23154 <100832619+DD23154@users.noreply.github.com>
2023-09-27 13:29:48 +0200

    Create sketch_jan09a.ino

---
+++ sketch_jan09a.ino
@@ -0,0 +1,49 @@
+#include <ros.h>
+#include <std_msgs/String.h>
+
+ros::NodeHandle nh;
+
+
+void messageCb(std_msgs::String & str_msg){
+
+  if(strcmp(str_msg.data, "yellow on")==0){
+    analogWrite(3, 120);
+  }
+  if(strcmp(str_msg.data, "yellow off")==0){
+    analogWrite(3, 0);
+  }
+  if(analogRead(3)!=0){
+    if(strcmp(str_msg.data, "higher")==0){
+    analogWrite(3, 255);
+    }
+  }
+  if(analogRead(3)!=0){
+    if(strcmp(str_msg.data, "lower")==0){
+    analogWrite(3, 60);
+    }
+  }
+  
+}
+
+
+ros::Subscriber<std_msgs::String> sub("chatter", &messageCb);
+
+void setup()
+{
+
+    pinMode(3, OUTPUT);
+    nh.initNode();
+    nh.subscribe(sub);
+
+}
+
+
+
+void loop()
+
+{
+
+    nh.spinOnce();
+    delay(1);
+
+}
